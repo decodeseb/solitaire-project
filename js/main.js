654ba5a82dce06beb9f5f59dc905a1879d08c486
decodeseb <seb9Villiers@gmail.com>
2025-10-25 19:28:49 -0400

    card stacking ability added with css

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 let deck_id="";
-
+let stock;
 fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) //parse response as JSON
       .then(data => {
         console.log(data)
-        deck_id = data.deck_id;
-        console.log(deck_id);
+        if(!localStorage.getItem("deck_id")){
+          deck_id = data.deck_id;
+          localStorage.setItem("deck_id", deck_id);
+        };
+
+        console.log(deck_id, localStorage.getItem("deck_id"));
       })
 
       .catch(err => {
                 console.log(`error ${err}`)
             });
 
+// Draw initial cards
+
+
+/* fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=7`)
+      .then(res => res.json()) //parse response as JSON
+      .then(data => {
+        console.log(data.deck_id, data.code);
+        for(let i=0 )
+      })
+
+      .catch(err => {
+                console.log(`error ${err}`)
+            });
+
+ */
