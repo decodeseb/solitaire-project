f8a936d545e30aad8be4a83b47074750c4c333c0
decodeseb <seb9Villiers@gmail.com>
2025-10-31 23:02:50 -0400

    created draw a card function with fetch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let deck_id="";
 let stock;
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+
+if(!localStorage.getItem("deck_id")){
+
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) //parse response as JSON
       .then(data => {
         console.log(data)
-        if(!localStorage.getItem("deck_id")){
           deck_id = data.deck_id;
           localStorage.setItem("deck_id", deck_id);
-        };
 
         console.log(deck_id, localStorage.getItem("deck_id"));
       })
@@ -16,18 +17,23 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
                 console.log(`error ${err}`)
             });
 
-// Draw initial cards
+} else {
+
+     deck_id = localStorage.getItem("deck_id");
+
+}
+
+console.log(deck_id, localStorage.getItem("deck_id"))
+// Draw a card
 
 
-/* fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=7`)
+fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
       .then(res => res.json()) //parse response as JSON
       .then(data => {
-        console.log(data.deck_id, data.code);
-        for(let i=0 )
+        console.log(data, data.cards[0]["image"]);
       })
 
       .catch(err => {
                 console.log(`error ${err}`)
             });
 
- */
