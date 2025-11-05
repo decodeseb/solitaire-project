2a5505d99b9acbd48f528b9671a6a84a3ea62107
decodeseb <seb9Villiers@gmail.com>
2025-11-04 19:35:02 -0500

    added tableau card distribution logic, with faceup for last card of each pile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let deck_id="";
-let stock;
+let stock = document.querySelector("div.stock img");
 
 if(!localStorage.getItem("deck_id")){
 
@@ -7,15 +7,15 @@ if(!localStorage.getItem("deck_id")){
       .then(res => res.json()) //parse response as JSON
       .then(data => {
         console.log(data)
-          deck_id = data.deck_id;
+        deck_id = data.deck_id;
 
-          localStorage.setItem("deck_id", deck_id);
+        localStorage.setItem("deck_id", deck_id);
 
         //draw a card if a deck doesnt exist
 
-          fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+        fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
               .then(res => res.json()) //parse response as JSON
-              .then(data => {
+              .then(data => { 
                 console.log(data, data.cards[0]["image"]);
               })
 
@@ -29,13 +29,14 @@ if(!localStorage.getItem("deck_id")){
                 console.log(`error ${err}`)
             });
 
-} else {
+} else { //if the deck already exists.
     deck_id = localStorage.getItem("deck_id");
     //draw a card
     fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
           .then(res => res.json()) //parse response as JSON
           .then(data => {
             console.log(data, data.cards[0]["image"]);
+            stock.src = data.cards[0]['image'];
           })
 
           .catch(err => {
@@ -44,4 +45,47 @@ if(!localStorage.getItem("deck_id")){
 
 }
 
+//tableau
 
+// data structure representing the tableau, array of subarrays.
+let tableau = [
+  [],  //pile 1
+  [],
+  [],
+  [],
+  [],
+  [],
+  []  //pile 7
+]; 
+
+class Card {
+  constructor(rank, suit, faceUp = false, image = null){
+    this.rank = rank; // 2-10, A, K, Q
+    this.suit = suit; // spades, 'hearts'...
+    this.faceUp = faceUp; // false by default
+    this.image = image; //url path (unknown at class creation time)
+  }
+
+  //methods 
+  flip() {
+    this.faceUp = !this.faceUp;
+  }
+}
+
+//dealing cards to tableau
+for(let i=0; i<tableau.length; i++){
+  for(let j=0; j < i+1; j++){
+    tableau[i].push(new Card("Test", 'Test', false, "test.png"));
+  }
+}
+
+//flip top-card
+for(let i=0; i<tableau.length; i++){
+  /* tableau[i][tableau[i].length-1]["faceUp"] = true; */ //not very readable
+  const pile = tableau[i];
+  const lastCard = pile[pile.length-1];
+  lastCard.faceUp = true;
+}
+
+
+console.log(tableau);
